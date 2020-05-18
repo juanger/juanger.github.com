918300e335031f0b1d2f6e12ad51bae70d660e90
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2020-05-18 01:28:46 -0700

    Add disqus comments

--- pages/[lang]/[year]/[month]/[slug].tsx
+++ pages/[lang]/[year]/[month]/[slug].tsx
@@ -11,21 +11,54 @@ export default function Post({
   categories,
   contents,
   lang,
+  slug,
 }) {
   return (
     <Layout>
       <PageHeader lang={lang}></PageHeader>
       <article>
         <h1>{title}</h1>
-        <time dateTime={date}></time>
+        <time dateTime={date}>
+          <h3>{date}</h3>
+        </time>
         <section dangerouslySetInnerHTML={{ __html: contents }} />
-        <blockquote>
-          <footer>
-            <h3>– Juan German Castañeda Echevarría</h3>
-            <time>{date}</time>
-          </footer>
-        </blockquote>
+        <footer>
+          <h3>– Juan German Castañeda Echevarría</h3>
+        </footer>
       </article>
+      <footer>
+        <section>
+          <div className="fullwidth" id="disqus_thread"></div>
+        </section>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              /**
+              *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+              *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+              
+              var disqus_config = function () {
+                this.page.url = 'http://blog.juanger.com/${lang}/${date}/${slug}';  // Replace PAGE_URL with your page's canonical URL variable
+                this.page.identifier = '${lang}/${date}/${slug}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                this.language = '${lang == "es" ? "es_MX" : "en_US"}';
+              };
+              
+              (function() { // DON'T EDIT BELOW THIS LINE
+              var d = document, s = d.createElement('script');
+              s.src = 'https://xocoruby.disqus.com/embed.js';
+              s.setAttribute('data-timestamp', +new Date());
+              (d.head || d.body).appendChild(s);
+              })();
+            `,
+          }}
+        />
+        <noscript>
+          Please enable JavaScript to view the{" "}
+          <a href="https://disqus.com/?ref_noscript">
+            comments powered by Disqus.
+          </a>
+        </noscript>
+      </footer>
     </Layout>
   );
 }
@@ -52,6 +85,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       categories: post.categories || [],
       comments: post.comments || false,
       date: new Date(post.date).toISOString().split("T")[0],
+      slug,
       lang,
     },
   };
